6af635785aa7c0eba6398764164195d1e7f8b0fc
Selahattin Ünlü <selahattin.unlu@yandex.com>
2016-02-24 17:33:21 +0200

    add lumen compatibility

---
+++ src/LumenServiceProvider.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Unlu\Laravel\Api;
+
+use Illuminate\Support\ServiceProvider;
+
+class LumenServiceProvider extends ApiQueryBuilderServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+}
