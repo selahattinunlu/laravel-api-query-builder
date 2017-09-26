d8a8a4c4f72300e82fb3f836b51523af137a213d
Helson Taveras <helsont@gmail.com>
2017-09-26 11:43:12 -0700

    Added first to query

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -114,6 +114,17 @@ class QueryBuilder
         return $result;
     }
 
+    public function first()
+    {
+        $result = $this->query->first();
+
+        if ($this->hasAppends()) {
+            $result = $this->addAppendsToModel($result);
+        }
+
+        return $result;
+    }
+
     public function lists($value, $key)
     {
         return $this->query->lists($value, $key);
